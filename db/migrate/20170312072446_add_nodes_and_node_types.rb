cc17e00b71df4fd1653b1d911949520aa8b75464
Txinto <txinto@gatatac.org>
2017-03-12 09:23:00 +0100

    Update node types: System -> Product SubSystem -> System

--- db/migrate/20170312072446_add_nodes_and_node_types.rb
+++ db/migrate/20170312072446_add_nodes_and_node_types.rb
@@ -9,9 +9,9 @@ class AddNodesAndNodeTypes < ActiveRecord::Migration
     end
 
     NodeType.create :name => "Library", :totype => "Library", :img_link => "/images/nodes/library.png"
-    NodeType.create :name => "System", :totype => "SubSystem", :img_link => "/images/nodes/system.png"
-    NodeType.create :name => "SubSystem", :totype => "SubSystem", :img_link => "/images/nodes/subsystem.png"
-    NodeType.create :name => "Parameter", :totype => "SubSystem", :img_link => "/images/nodes/param.png"
+    NodeType.create :name => "Product", :totype => "System", :img_link => "/images/nodes/product.png"
+    NodeType.create :name => "System", :totype => "System", :img_link => "/images/nodes/system.png"
+    NodeType.create :name => "Parameter", :totype => "System", :img_link => "/images/nodes/param.png"
     NodeType.create :name => "Value", :totype => "Value", :img_link => "/images/nodes/value.png"
     NodeType.create :name => "Mode", :totype => "Mode", :img_link => "/images/nodes/mode.png"
     NodeType.create :name => "Group", :totype => "Group", :img_link => "/images/nodes/system2.png"
--- db/migrate/20170312073617_add_user_abbrev.rb
+++ db/migrate/20170312073617_add_user_abbrev.rb
@@ -1,6 +1,12 @@
 class AddUserAbbrev < ActiveRecord::Migration
   def self.up
     add_column :users, :abbrev, :string
+
+    #TODO: Add an script to update users to have a default abbreviation
+ #	User.all.each {|u|
+#		u.abbrev = u.name 
+#		u.save
+#	}
   end
 
   def self.down
